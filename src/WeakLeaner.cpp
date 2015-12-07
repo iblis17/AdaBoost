4f0942dab0e053c012f5058ec09349d595e5dc2b
Iblis Lin <e196819@hotmail.com>
2015-12-07 17:36:17 +0800

    Fix indentation for WeakLearner.hpp

--- src/WeakLeaner.cpp
+++ src/WeakLeaner.cpp
@@ -6,38 +6,38 @@
 
 int main()
 {
-	float pf[1][4] = {
-		{50, 20, 84, 30}
-	};
-	float pw[1][4] = {
-		{0.1, 0.1, 0.1, 0.1}
-	};
-	float nf[1][6] = {
-		{42, 80, 104, 5, 70, 65}
-	};
-	float nw[1][6] = {
-		{0.1, 0.1, 0.1, 0.1, 0.1, 0.1}
-	};
-	int pf_shape[2] = { 1, 4 };
-	int nf_shape[2] = { 1, 6 };
+    float pf[1][4] = {
+        {50, 20, 84, 30}
+    };
+    float pw[1][4] = {
+        {0.1, 0.1, 0.1, 0.1}
+    };
+    float nf[1][6] = {
+        {42, 80, 104, 5, 70, 65}
+    };
+    float nw[1][6] = {
+        {0.1, 0.1, 0.1, 0.1, 0.1, 0.1}
+    };
+    int pf_shape[2] = { 1, 4 };
+    int nf_shape[2] = { 1, 6 };
     float ret[1][3];
 
-	Compute c("WeakLearn");
+    Compute c("WeakLearn");
 
-	c.set_buffer((float *)pf, 1 * 4);
-	c.set_buffer((float *)nf, 1 * 6);
+    c.set_buffer((float *)pf, 1 * 4);
+    c.set_buffer((float *)nf, 1 * 6);
 
-	c.set_buffer((float *)pw, 1 * 4);
-	c.set_buffer((float *)nw, 1 * 6);
+    c.set_buffer((float *)pw, 1 * 4);
+    c.set_buffer((float *)nw, 1 * 6);
 
-	c.set_buffer((int *)pf_shape, 2);
-	c.set_buffer((int *)nf_shape, 2);
+    c.set_buffer((int *)pf_shape, 2);
+    c.set_buffer((int *)nf_shape, 2);
 
-	c.set_ret_buffer((float *)ret, 1 * 3);
+    c.set_ret_buffer((float *)ret, 1 * 3);
 
-	c.run(1, 4);
+    c.run(1, 4);
 
-	std::cout << ret[0][0] << std::endl;
-	std::cout << ret[0][1] << std::endl;
-	std::cout << ret[0][2] << std::endl;
+    std::cout << ret[0][0] << std::endl;
+    std::cout << ret[0][1] << std::endl;
+    std::cout << ret[0][2] << std::endl;
 }
