878717090fa4a165d37a131b29ed220a17ada26d
Iblis Lin <e196819@hotmail.com>
2015-12-07 17:35:41 +0800

    Fix indentation for kernel.hpp

--- src/compute/kernel.hpp
+++ src/compute/kernel.hpp
@@ -6,24 +6,25 @@ const std::string Compute::kernel_src = R"(
 
 kernel void add(global float *a, global float *b, global float *ret)
 {
-	const int row = get_global_id(0);
+    const int row = get_global_id(0);
 
-	ret[row] = a[row] + b[row];
+    ret[row] = a[row] + b[row];
 }
 
+
 kernel void WeakLearn(
-	global float *pf_maxtrix, global float *nf_maxtrix,
+    global float *pf_maxtrix, global float *nf_maxtrix,
     global float *pf_weight, global float *nf_weight,
-	global read_only int *pf_shape, global read_only int *nf_shape,
+    global read_only int *pf_shape, global read_only int *nf_shape,
     global float *ret_matrix)
 {
-	/*
-	 * :param pf_maxtrix:
-	 * 		float matrix, feature_size x sample_size
+    /*
+     * :param pf_maxtrix:
+     *         float matrix, feature_size x sample_size
      * :param ret_matrix:
      *      feature_size x 3 (error, polarity, theta)
-	 * */
-	const int row = get_global_id(0);
+     * */
+    const int row = get_global_id(0);
     const int pf_col = *(pf_shape + 1);
     const int nf_col = *(nf_shape + 1);
 
@@ -38,8 +39,8 @@ kernel void WeakLearn(
     int slice = 10;
 
     float error = 1;
-	float theta = 0;
-	float polarity = 1;
+    float theta = 0;
+    float polarity = 1;
 
     // find the max/min from (pf + nf)
     for (int i=1; i<pf_col; ++i)
@@ -53,11 +54,11 @@ kernel void WeakLearn(
         min_ = min(min_, nf[i]);
     }
 
-	for (int i=1; i<slice; ++i)
-	{
-		float theta1 = (max_ - min_) * i / (slice - 1);
-		float error1 = 0;
-		float polarity1 = 1;
+    for (int i=1; i<slice; ++i)
+    {
+        float theta1 = (max_ - min_) * i / (slice - 1);
+        float error1 = 0;
+        float polarity1 = 1;
         /*
          *   negative data  |  positive data
          *                  |
@@ -66,32 +67,32 @@ kernel void WeakLearn(
          *  polarity = -1 <-|-> polarity = 1
          */
 
-		for (int j=0; j<pf_col; ++j)
-			if (*(pf + j) < theta1)
-				error1 += pw[j];
+        for (int j=0; j<pf_col; ++j)
+            if (*(pf + j) < theta1)
+                error1 += pw[j];
 
         for (int j=0; j<nf_col; ++j)
-			if (*(nf + j) > theta1)
-				error1 += nw[j];
+            if (*(nf + j) > theta1)
+                error1 += nw[j];
 
         if (error1 > 0.5)
-		{
-			polarity1 = -1;
-			error1 = 1 - error1;
-		}
+        {
+            polarity1 = -1;
+            error1 = 1 - error1;
+        }
 
         if (error1 < error)
-		{
-			error = error1;
-			polarity = polarity1;
-			theta = theta1;
-		}
-	}
+        {
+            error = error1;
+            polarity = polarity1;
+            theta = theta1;
+        }
+    }
 
-	// return
-	ret[0] = error;
-	ret[1] = polarity;
-	ret[2] = theta;
+    // return
+    ret[0] = error;
+    ret[1] = polarity;
+    ret[2] = theta;
 }
 )";
 
