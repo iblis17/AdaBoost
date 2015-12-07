414ec45de731a8e933d9d41a533da023d9aac8f4
Iblis Lin <e196819@hotmail.com>
2015-12-07 21:59:40 +0800

    [Add.cpp] Update example

--- src/Add.cpp
+++ src/Add.cpp
@@ -9,7 +9,7 @@ int main()
 	float test[4] = {1, 2, 3, 4};
 	float test2[4] = {5, 6, 7, 8};
 	float ret[4];
-	Compute c("add");
+	Compute c("add", CL_DEVICE_TYPE_CPU);
 
 	c.set_buffer(test, 4);
 	c.set_buffer(test2, 4);
