c5b6bb413ef04912e55030653b2beb21ea6d7475
Iblis Lin <e196819@hotmail.com>
2016-04-15 16:49:01 +0800

    [Add] assertion

--- src/Add.cpp
+++ src/Add.cpp
@@ -1,5 +1,7 @@
 #include <cstdio>
 #include <iostream>
+#include <cassert>
+
 
 #include "compute/compute.hpp"
 
@@ -8,6 +10,7 @@
 #else
     #define DEV_TYPE CL_DEVICE_TYPE_CPU
 #endif
+
 
 int main()
 {
@@ -22,12 +25,22 @@ int main()
 	c.run(4);
 
 	for(auto &i: ret)
-		std::cout << i << std::endl;
+        std::cout << i << std::endl;
+    assert(ret[0] == 6);
+    assert(ret[1] == 8);
+    assert(ret[2] == 10);
+    assert(ret[3] == 12);
 
+    // test for reset_buffer
     test[0] = 100;
     c.reset_buffer(0, test);
 	c.run(4);
 
     for(auto &i: ret)
-		std::cout << i << std::endl;
+        std::cout << i << std::endl;
+    assert(ret[0] == 105);
+    assert(ret[1] == 8);
+    assert(ret[2] == 10);
+    assert(ret[3] == 12);
+
 }
